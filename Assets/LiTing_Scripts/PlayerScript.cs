ec37cfb808e3b583f91be043db555e550797d127
03-LiTingLim <91869598+03-LiTingLim@users.noreply.github.com>
2021-10-12 20:57:32 +0800

    Added Score

--- Assets/LiTing_Scripts/PlayerScript.cs
+++ Assets/LiTing_Scripts/PlayerScript.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
     public ParticleSystem ps;
     public ParticleSystem explosion;
+    public Text scoreText;
+
+    private int scoreCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText.GetComponent<Text>().text = "Score : 0 ";
     }
 
     // Update is called once per frame
@@ -28,6 +32,8 @@ public class PlayerScript : MonoBehaviour
 
         if (collision.gameObject.tag.Equals("Coin"))
         {
+            scoreCount += 10;
+            scoreText.GetComponent<Text>().text = "Score : " + scoreCount;
             explosion.Play();
         }
     }
